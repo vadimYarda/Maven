9994ffa08ce74ee9e0f07e14c2fad587305c3ad4
Vados test <vadimyarda27@gmail.com>
2024-06-08 18:38:11 +0300

    Hw

---
+++ src/main/java/DbSetting.java
@@ -0,0 +1,15 @@
+import java.util.UUID;
+
+public class DbSetting {
+
+    private String name;
+    private String password;
+
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+}
+
+
